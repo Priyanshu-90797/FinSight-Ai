bb5bcea7c855f88a2ebc77afaada1130c7b920d4
Priyanshu khandelwal <Priyanshukhandelwal1912@gmail.com>
2026-04-26 20:21:36 +0530

    config.py

--- config.py
+++ config.py
@@ -1,16 +1,12 @@
-# ---------------- APP CONFIG ----------------
 APP_TITLE = "FinSight AI"
 APP_SUBTITLE = "AI-Powered Personal Finance Advisor"
 APP_ICON = "💎"
 
-# ---------------- THEME ----------------
 THEME = "dark"
 
-# ---------------- COLORS ----------------
 COLOR_PALETTE = [
     "#8B5CF6", "#10b981", "#38bdf8",
     "#f59e0b", "#f43f5e"
 ]
 
-# ---------------- FINANCE ----------------
 HEALTHY_SAVINGS_RATE = 0.2
